a6ab889b83a72616784ea2ebf4fab2d6c0e5779b
Suraj Deshmukh <surajd.service@gmail.com>
2018-07-27 10:20:16 +0530

    initial commit

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/surajssd/samplecode/math"
+)
+
+func main() {
+	fmt.Println(math.Add(1, 2))
+}
---
+++ math/math_test.go
@@ -0,0 +1,20 @@
+package math
+
+import "testing"
+
+func TestAdd(t *testing.T) {
+	tcs := []struct {
+		a      float64
+		b      float64
+		result float64
+	}{
+		{1, 2, 3},
+		{5, -7, -1},
+	}
+
+	for _, tc := range tcs {
+		if Add(tc.a, tc.b) != tc.result {
+			t.Errorf("the output does not match for %f+%f=%f", tc.a, tc.b, tc.result)
+		}
+	}
+}
